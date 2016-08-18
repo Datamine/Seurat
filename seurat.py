714899030bd7974029c08f31b8dd2e12dfb68fb6
Datamine <contact@johnloeber.com>
2016-08-17 23:54:07 -0500

    first draft going ok

--- seurat.py
+++ seurat.py
@@ -7,11 +7,6 @@ import numpy as np
 
 import Image
 
-#img = imread(sys.argv[1])[:, :, :3]
-# denoised = denoise_bilateral(img, sigma_range=0.1, sigma_spatial=35)
-
-im = Image.new("RGB", (256,256), "white")
-
 def generate_random_point_around(seed, lower_bound_radius, upper_bound_radius):
     """
     generate a point uniformly around a given seed point, from the 
@@ -25,44 +20,62 @@ def generate_random_point_around(seed, lower_bound_radius, upper_bound_radius):
     offset = np.array([radius * math.sin(angle), radius*math.cos(angle)])
     return tuple(seed + offset)
 
+def no_neighbors(point, sample_points, tree, mindist):
+    """
+    returns True if a new point has no neighbors within the mindist distance.
+        point : tuple (float, float)
+        tree : cKDTree
+        mindist : float
+    """
+    distances, indexes = tree.query(point, len(sample_points) + 1, distance_upper_bound = mindist)
+    if len(distances)==0:
+        return False
+    for dist, index in zip(distances, indexes):
+        if np.isinf(dist):
+            return True
+        if dist < mindist:
+            return False
 
 def get_poisson_points(image, minimum_distance_between_samples, radius):
-    h = im.size[0]
-    w = im.size[1]
+    """
+    runs the poisson disc algorithm to generate sample points.
+    """
+    h = image.size[0]
+    w = image.size[1]
     first_point = (random.uniform(0, h), random.uniform(0,w))
     to_process = [first_point]
     sample_points = [first_point]
     tree = cKDTree(sample_points)
 
-    def no_neighbors(point):
-        distances, indexes = tree.query(point, len(sample_points) + 1, distance_upper_bound = minimum_distance_between_samples)
-        if len(distances)==0:
-            return False
-        for dist, index in zip(distances, indexes):
-            if np.isinf(dist):
-                return True
-            if dist < minimum_distance_between_samples:
-#                print "thisa actually happens"
-                return False
-
     while to_process:
         pt = to_process.pop(random.randrange(len(to_process)))
         for _ in range(30):
-            new_point = generate_random_point_around(pt, radius, 2* radius)
-            if (0 <= new_point[0] < h) and (0 <= new_point[1] < w) and (no_neighbors(new_point)):
+            new_point = generate_random_point_around(pt, radius, 2*radius)
+            if (0 <= new_point[0] < h) and (0 <= new_point[1] < w) and \
+            (no_neighbors(new_point, sample_points, tree, minimum_distance_between_samples)):
                 to_process.append(new_point)
                 sample_points.append(new_point)
                 tree = cKDTree(sample_points)
     return sample_points
 
-sample_points = get_poisson_points(im, 20, 30)
+r = 6
+mind = 10
 
-draw = ImageDraw.Draw(im)
-def makecircle(image, coord, rad):
-    draw.ellipse((coord[0] - rad, coord[1] - rad, coord[0] + rad, coord[1] + rad), outline = (255,0,0,0))
+def makecircle(image, coord, rad, color):
+    draw.ellipse((coord[0] - rad, coord[1] - rad, coord[0] + rad, coord[1] + rad), fill = color)
 
-for point in sample_points:
-    new = (int(point[0]), int(point[1]))
-    makecircle(im, new, 10)
+opened = Image.open("bear.jpg")
+new = Image.new("RGB", opened.size, "white")
+draw = ImageDraw.Draw(new)
 
-im.save("img2.png")
+# five repetitions
+for i in range(5):
+    print i
+    sample_points = get_poisson_points(opened, 2*r, mind)
+
+    for point in sample_points:
+        samplecolor = opened.getpixel(point)
+        new_point = (int(point[0]), int(point[1]))
+        makecircle(new, new_point, r, samplecolor)
+
+new.save("output.png")
